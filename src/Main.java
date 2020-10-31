21f3303127abb3d27a0b7dd2bf9db302f215d176
Niv Meiri <nivmeir@post.bgu.ac.il>
2020-11-01 00:44:04 +0200

    Play_With_oreders

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
+import java.sql.Date;
 import java.util.List;
 
 public class Main {
@@ -12,8 +13,12 @@ public class Main {
         Supplier s=new Supplier("312","nim");
         Product p=new Product("111","CoffeMachine");
         p.setSupplier(s);
+        Order o = new Order("8765", Date.valueOf("2015-03-30"), Date.valueOf("2015-03-30"), new Address(), OrderStatus.Hold, 9999);
+        //LineItem l=new LineItem(100,9999,o,p);
+        p.setSupplier(s);
         System.out.println(p.getSupplier().getId());
         System.out.println(p.getId());
+        System.out.println(o.toString());
 
         /*
         public void  Add(String Login_id){
