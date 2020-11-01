8d14ecd415b4441907444a27d57f9ca43185e661
hadassaz <hadassaz@post.bgu.ac.il>
2020-11-01 19:32:16 +0200

    בעה

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,8 @@
+import com.sun.org.apache.bcel.internal.generic.GOTO;
+
 import java.sql.Date;
 import java.util.List;
+import java.util.Scanner;
 
 public class Main {
     private List<WebUser> WebUsers;
@@ -26,6 +29,39 @@ public class Main {
 
         }
         */
+        public void MakeOrder(){
+            Scanner sciny=new Scanner(System.in);
+            System.out.println("Who would you to order from?: ");
+            String name=sciny.nextLine();
+            WebUser MyWeb =this.WebUsers[name];//using the hashmap
+            MyWeb.getShoppingCart().printProducts();
+            System.out.println("Would you like to order one of our products? (y/n)");
+            String ans = sciny.nextLine();
+            while (ans != "n"){
+                System.out.println("What product do you want to add to your Cart?: ");
+                String item = sciny.nextLine();
+                int index=-1;
+                int degel=1;
+                while (degel==1) {
+                    for (int i = 0; i < MyWeb.getShoppingCart().getLineItems().size(); i++) {
+                        if (MyWeb.getShoppingCart().getLineItems().get(i).getProduct().getName().equals(item)) {
+                            index = i;
+                            degel=0;
+                            break;
+                        }
+                    }
+                    if (index == -1)
+                        System.out.println("Please enter valid name!");
+                }
+
+
+
+
+                }
+
+            }
+
+        }
 
     }
 
