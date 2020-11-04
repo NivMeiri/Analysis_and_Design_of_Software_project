abc36b66d4624cca2b1518cdd370a44f8a30788c
Niv Meiri <nivmeir@post.bgu.ac.il>
2020-11-04 20:41:29 +0200

    harta

--- src/System1.java
+++ src/System1.java
@@ -1,21 +1,23 @@
 import jdk.nashorn.internal.objects.Global;
 
-import java.util.Calendar;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Scanner;
+import java.util.*;
 import java.sql.Date;
 public class System1 {
     private HashMap<String, WebUser> Webusers;
-    private WebUser CurrentWebUser=null;
+    private WebUser CurrentWebUser = null;
     private List<Order> Oreders;
-    private Order LastOreder=null;
-    private int OrderNum=0;
-    private List<Product>  Pruducts;
-    public System1(){
-        this.CurrentWebUser=null;
-        this.LastOreder=null;
+    private Order LastOreder = null;
+    private int OrderNum = 0;
+    private Dictionary<String, Supplier> Suppliers;
+    private List<Product> Products;
+    private HashMap<Object, Integer> AllObjInSys;
+    private int id = 0;
+
+    public System1() {
+        this.CurrentWebUser = null;
+        this.LastOreder = null;
     }
+
     /*
     1) take input
     2) create a customer with null Account
@@ -24,38 +26,38 @@ public class System1 {
     5) create ShoppingCart With WebUser, set cart to Account
 
     */
-    public void  Add_WebUser(String ID){
+    public void Add_WebUser(String ID) {
         System.out.println("Enter your password: ");
         Scanner s = new Scanner(System.in);
         String Pass = s.nextLine();
         System.out.println("Are you a Premium User? (y/n)");
         String YesOrNo = s.nextLine();
         System.out.println("Please enter your addres: ");
-        Address newAddres = new Address("street",1,"city","state",32942);          /////TODO fix here///////
-        String myAddress=s.nextLine();
+        Address newAddres = new Address("street", 1, "city", "state", 32942);          /////TODO fix here///////
+        String myAddress = s.nextLine();
         System.out.println(myAddress);
         System.out.println("Please enter your Phone Number: ");
         String pNumber = s.nextLine();
         System.out.println("Please enter your Email: ");
         String eMail = s.nextLine();
-        Customer newCustomer = new Customer(ID,newAddres,pNumber,eMail,null);
-        Account a1=null;
-        switch (YesOrNo){
+        Customer newCustomer = new Customer(ID, newAddres, pNumber, eMail, null);
+        Account a1 = null;
+        switch (YesOrNo) {
 
             case "y":
-                 a1 = new PremiumAccount(ID,"new addressi",false,new  Date(2020,10,1),null,
-                        0,newCustomer,null);
-                 newCustomer.SetAccount(a1);
-                 break;
+                a1 = new PremiumAccount(ID, "new addressi", false, new Date(2020, 10, 1), null,
+                        0, newCustomer, null);
+                newCustomer.SetAccount(a1);
+                break;
             case "n":
-                 a1 = new Account(ID,"newAddres",false,new Date(2019,10,8),null,
-                        0,newCustomer,null);
+                a1 = new Account(ID, "newAddres", false, new Date(2019, 10, 8), null,
+                        0, newCustomer, null);
                 newCustomer.SetAccount(a1);
                 break;
         }
 
-        WebUser myUser = new WebUser(ID,Pass,UserState.New,newCustomer);
-        ShoppingCart shop1 = new ShoppingCart(new Date(2020,9,9),myUser,a1);
+        WebUser myUser = new WebUser(ID, Pass, UserState.New, newCustomer);
+        ShoppingCart shop1 = new ShoppingCart(new Date(2020, 9, 9), myUser, a1);
         if (a1 != null) {
             a1.setShoppingCart(shop1);
             myUser.setShoppingCart(shop1);
@@ -66,79 +68,85 @@ public class System1 {
         System.out.println(myUser.toString());
         System.out.println(newCustomer.toString());
     }
-    public void Remove_Webuser(String Login_id){
+
+    public void Remove_Webuser(String Login_id) {
         this.Webusers.get(Login_id).delete();
     }
-    public void Login(String Login_id){
-        if(this.Webusers.get(Login_id)==null) {
+
+    public void Login(String Login_id) {
+        if (this.Webusers.get(Login_id) == null) {
             System.out.println("The user is not exist!!");
         }
         Scanner s = new Scanner(System.in);
         System.out.println("Please enter your Password");
         String Pass = s.nextLine();
     }
-    public void LogOut(){
+
+    public void LogOut() {
     }
-    public void order(){
+
+    public void order() {
     }
-    public void MakeOrder(){
-        Order myOrder=new Order(String.valueOf(OrderNum),new Date(2000,10,18),null,this.CurrentWebUser.getCustomer().getAddress(),OrderStatus.New,0,this.CurrentWebUser.getCustomer().getAccount());
+
+    public void MakeOrder() {
+        Order myOrder = new Order(String.valueOf(OrderNum), new Date(2000, 10, 18), null, this.CurrentWebUser.getCustomer().getAddress(), OrderStatus.New, 0, this.CurrentWebUser.getCustomer().getAccount());
         OrderNum++;
-        Scanner sciny=new Scanner(System.in);
+        Scanner sciny = new Scanner(System.in);
         /// TODO CHECK IF THE WEBUSER EXIST ///
         WebUser MyWeb;
         while (true) {
             System.out.println("Who would you like to order from?: ");
             String name = sciny.nextLine();
             MyWeb = this.Webusers.get(name);//using the hashmap
-            if (MyWeb !=null)
+            if (MyWeb != null)
                 break;
             System.out.println("Please enter valid name");
         }
         MyWeb.getShoppingCart().printProducts();
         /// TODO CHECK IF THERE IS A PRODUCTS ///
-        if(MyWeb.getShoppingCart().numberOfLineItems()==0){
+        if (MyWeb.getShoppingCart().numberOfLineItems() == 0) {
             System.out.println("The WebUser has no products");
             return;
         }
 
         System.out.println("Would you like to order one of our products? (y/n)");
         String ans = sciny.nextLine();
-        while (ans == "y"){
+        while (ans == "y") {
             System.out.println("What product do you want to add to your Cart?: ");
             String item = sciny.nextLine();
-            LineItem UserItem=null;
+            LineItem UserItem = null;
             int num;
-            while (UserItem==null) {
+            while (UserItem == null) {
                 for (int i = 0; i < MyWeb.getShoppingCart().getLineItems().size(); i++) {
                     if (MyWeb.getShoppingCart().getLineItems().get(i).getProduct().getName().equals(item)) {
-                        UserItem=MyWeb.getShoppingCart().getLineItems().get(i);
-                        break; } }
-                if (UserItem==null)
+                        UserItem = MyWeb.getShoppingCart().getLineItems().get(i);
+                        break;
+                    }
+                }
+                if (UserItem == null)
                     System.out.println("Please enter a valid name!");
             }
             while (true) {
                 System.out.println("How many units do you want?");
                 num = Integer.parseInt(sciny.nextLine());
                 if (UserItem.getQuantity() >= num) {
-                    if (myOrder.getTotal()+(UserItem.getPrice()*num)<this.CurrentWebUser.getCustomer().getAccount().getBalance())
+                    if (myOrder.getTotal() + (UserItem.getPrice() * num) < this.CurrentWebUser.getCustomer().getAccount().getBalance())
                         break;
                     System.out.println("You don't have enough money, try ordering less");
-                }
-                else {
+                } else {
                     System.out.println("Please enter valid number!");
                 }
             }
-            LineItem myItem=new LineItem(num,UserItem.getPrice(),this.CurrentWebUser.getShoppingCart(),myOrder,UserItem.getProduct());
-            UserItem.setQuantity(UserItem.getQuantity()-num);
+            LineItem myItem = new LineItem(num, UserItem.getPrice(), this.CurrentWebUser.getShoppingCart(), myOrder, UserItem.getProduct());
+            UserItem.setQuantity(UserItem.getQuantity() - num);
             myOrder.addLineItem(myItem);
-            myOrder.setTotal(myOrder.getTotal()+myItem.getPrice()*num);
+            myOrder.setTotal(myOrder.getTotal() + myItem.getPrice() * num);
             this.CurrentWebUser.getShoppingCart().addLineItem(myItem);
             System.out.println("Would you like to order something else? y/n");
-            ans=sciny.nextLine();
+            ans = sciny.nextLine();
         }
         System.out.println("Do you want to pay now? y/n");
-        ans=sciny.nextLine();
+        ans = sciny.nextLine();
         Payment myPayment;
         switch (ans) {
             case "y":
@@ -149,34 +157,77 @@ public class System1 {
             case "n":
                 System.out.println("When do you want to pay?");
                 System.out.println("insert day between 1-31:");
-                String day=sciny.nextLine();
+                String day = sciny.nextLine();
                 System.out.println("insert month between 1-12:");
-                String month=sciny.nextLine();
+                String month = sciny.nextLine();
                 System.out.println("insert year between 2020-2021:");
-                String year=sciny.nextLine();
-                myPayment=new DelayedPayment(this.CurrentWebUser.getLogin_id(), date, myOrder.getTotal(), "", MyWeb, myOrder);
+                String year = sciny.nextLine();
+                myPayment = new DelayedPayment(this.CurrentWebUser.getLogin_id(),new Date(2020,3,7), myOrder.getTotal(), "", MyWeb.getCustomer().getAccount(), myOrder);
                 myOrder.addPayment(myPayment);
                 break;
             default:
         }
     }
 
-    public void Check_Func(){
-<<<<<<< HEAD
+    public void Check_Func() {
         System.out.println("Working?");
     }
-    public void Display_Order(){
-    }
-   public void Niv_Branch(){};
-=======
-        System.out.println("Working?"); }
-    public void Display_Order(){ }
->>>>>>> Hadassa_Branch
-    public void Link_Product(){}
-    public void AddProduct(){}
-    public void Delete_Product(){}
-    public void Show_all_Objects(){
-    }
-    public void Show_object_id(){}
 
+
+    public void Display_Order() {
+    }
+
+    public void Link_Product() {
+    }
+
+    public void AddProduct() {
+    }
+
+    public void Delete_Product(String Product_name) {
+        //if no product's name was given. ?
+        if (Product_name.length() == 0) {
+            System.out.println("No product name is written. Please try again");
+            return;
+        }
+        boolean foundAny = false;
+        for (Product p : Products) {
+            if (p.getName().equals(Product_name)) {
+                foundAny = true;
+                p.delete();
+            }
+        }
+        //if name given isn't an existed product's name in system, then try again.
+        if (!foundAny) {
+            System.out.println("Product does not exist. Please try again")
+        } ;
+    }
+
+    public void Show_all_Objects() {
+        System.out.println("| Object |  id  |  name  |");
+        Set<Object> objArr = AllObjInSys.keySet();
+        for (Object obj : objArr) {
+            if (obj instanceof Product) {
+                System.out.println("| " + obj.getClass().getName() + " | " + AllObjInSys.get(obj) + " | " + ((Product) obj).getName() + " |");
+            } else if (obj instanceof Order) {
+                System.out.println("| " + obj.getClass().getName() + " | " + AllObjInSys.get(obj) + " | " + ((Order) obj).getNumber() + " |");
+            } else if (obj instanceof WebUser) {
+                System.out.println("| " + obj.getClass().getName() + " | " + AllObjInSys.get(obj) + " | " + ((WebUser) obj).getLogin_id() + " |");
+            } else if (obj instanceof Customer) {
+                System.out.println("| " + obj.getClass().getName() + " | " + AllObjInSys.get(obj) + " | " + ((Customer) obj).getId() + " |");
+            }
+            //-------------TODO how many shall i do?-------
+        }
+    }
+
+    public void Show_object_id(int id) {
+        Set<Object> objArr = AllObjInSys.keySet();
+        for (Object obj : objArr) {
+            if (AllObjInSys.get(obj).equals(id)) {
+                System.out.println("System ID: " + id);
+                System.out.println(obj.toString());
+                return;
+            }
+        }
+    }
 }
+
